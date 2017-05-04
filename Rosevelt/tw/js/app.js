0b2f0a22656a03f348ff46387edfd9859d4ee0e9
Rosevelt Barahona <angularjs@ZiMac03.local>
2017-05-04 22:01:02 +0200

    tw3

--- Rosevelt/tw/js/app.js
+++ Rosevelt/tw/js/app.js
@@ -1,4 +1,4 @@
-angular.module('app', ['ngRoute']);
+/*angular.module('app', ['ngRoute']);
 angular.module('app').config(function($routeProvider) {
 
     $routeProvider
@@ -23,6 +23,59 @@ angular.module('app').config(function($routeProvider) {
             controllerAs: "addTuit"
         })
         .otherwise({
-			templateUrl: "views/404.html"
-		});
-});
+            templateUrl: "views/404.html"
+        });
+});*/
+
+//
+angular.module('app', ['ui.router'])
+    .config(function($stateProvider, $urlRouterProvider) {
+
+        $urlRouterProvider.otherwise('/');
+
+        $stateProvider
+            .state('home', {
+                views: {
+                    "topbar": {
+                        templateUrl: "views/topbar.html"
+                    },
+                    "content": {
+                        templateUrl: "views/tuit-list.html"
+                            //controlador
+                    },
+                    "footer": {
+                        templateUrl: "views/footer.html"
+                    }
+
+                },
+                viewCache: true,
+                abstract: true
+            })
+            .state('home.users', {
+                url: "/",
+                views: {
+                    "content": {
+                        templateUrl: "views/users-list.html",
+                        controller: "users",
+                        controllerAs: "users"
+                    }
+                }
+
+            })
+            .state('home.addTuit', {
+                url: "/addTuit",
+                views: {
+                    "content": {
+                        templateUrl: "views/add-tuit.html",
+                        controller: "addTuit",
+                        controllerAs: "addTuit"
+                    }
+                }
+
+            })
+
+    });
+
+
+
+
